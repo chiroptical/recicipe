05f4374d09779afc056002728fcf98877c6ef022
Barry Moore <chiroptical@gmail.com>
2021-01-07 20:36:33 -0500

    Use nix haskell packages to build project

---
+++ backend/shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import ../nix/pkgs.nix {} }:
+
+let
+  haskellPackages = (hp: with hp; [
+    hspec
+    hspec-megaparsec
+    megaparsec
+    parser-combinators
+  ]);
+
+  ghc = pkgs.haskell.packages.ghc884.ghcWithHoogle haskellPackages;
+
+in
+  pkgs.mkShell {
+    buildInputs = [
+      ghc
+      pkgs.cabal-install
+      pkgs.cabal2nix
+      pkgs.ghcid
+      pkgs.haskellPackages.hpack
+    ];
+  }
